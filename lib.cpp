ec6ad0fadf373431f9494f1fba745372f7764966
ValentinaStella <91681272+ValentinaStella@users.noreply.github.com>
2022-12-11 15:47:29 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
+#include "lib.h"
+#include <iostream>
+using namespace std;
+void trovamax(){
+    cout<<"Dimensione: "<<endl;
+    int D, max;
+    cin>>D;
+   int a[D];
+    for (int i = 0; i < D; ++i) {
+        cout<<"inserisci numeri: ";
+        cin>>a[i];
 
+
+    }
+    max=a[0];
+    for (int i = 0; i < D; ++i) {
+        if (a[i]>max){
+            max=a[i];
+        }
+
+    }
+    cout<<"massimo: "<<max<<endl;
+}
